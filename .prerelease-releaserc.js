02372aa1ccb2c55139beed5ea5611859e3ede803
mitzyvalenciaordonez <mitzyvo@gmail.com>
2025-05-27 15:07:42 -0500

    :sparkles: Added main setting files

---
+++ .prerelease-releaserc.js
@@ -0,0 +1,90 @@
+const { promisify } = require('util')
+const dateFormat = require('dateformat')
+const path = require('path')
+const TEMPLATE_DIR = path.join(__dirname, 'templates')
+const readFileAsync = promisify(require('fs').readFile)
+const template = readFileAsync(path.join(TEMPLATE_DIR, '/default-template.hbs'))
+const commitTemplate = readFileAsync(path.join(TEMPLATE_DIR,'/commit-template.hbs'))
+module.exports = {
+
+  branches: [
+    "main",
+    {
+      name: 'develop',
+      prerelease: true
+    }
+  ],
+  plugins: [
+    [
+      'semantic-release-gitmoji', {
+        releaseRules: {
+          major: [':boom:'],
+          minor: [':sparkles:'],
+          patch: [
+            ':bug:',
+            ':zap:',
+            ':ambulance:',
+            ':lock:',
+            ':hammer:',
+            ':white_check_mark:',
+            ':right_arrow_curving_left:',
+          ]
+        },
+        releaseNotes: {
+          template,
+          partials: {
+            commitTemplate
+          },
+          helpers: {
+            formatDate: function(date){
+              return dateFormat(date, 'yyy-mm-dd HH:MM:ss');
+            },
+            split: function(string){
+              return string.trim().split('\n');
+            },
+            formatDateCol: function(date) {
+              if(!date) date = new Date();
+              let formattedDate = date.toLocaleString('en-US', { 
+                timeZone: 'America/Bogota', 
+                year: 'numeric', 
+                month: '2-digit', 
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                hour12: false
+              }).replace(',','');
+              return `${formattedDate[2]}-${formattedDate[0]}-${formattedDate[1]} ${formattedDate[3]}:${formattedDate[4]}:${formattedDate[5]}`;
+            }
+          }
+        }
+      }
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        "prepareCmd": "mvn version:set -DnewVersion=\"${nextRelease.version}\" && mvn clean install"
+      }
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/prerelease/CHANGELOG.md",
+        "changelogTitle": ['# Gitmoji Prerelease Changelog', '\uD83C\uDF88'].join(' ')
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "**/pom.xml",
+          "docs/prerelease/PRERELEASE-CHANGELOG.md"
+        ],
+        message: [
+          ':bookmark: ${nextRelease.version} [skip ci]'
+        ].join('')
+      }
+    ]
+  ],
+  tagFormat: '${version}'
+}
---
+++ .releaserc.js
@@ -0,0 +1,78 @@
+const { promisify } = require('util')
+const dateFormat = require('dateformat')
+const path = require('path')
+const TEMPLATE_DIR = path.join(__dirname, 'templates')
+const readFileAsync = promisify(require('fs').readFile)
+const template = readFileAsync(path.join(TEMPLATE_DIR, '/default-template.hbs'))
+const commitTemplate = readFileAsync(path.join(TEMPLATE_DIR,'/commit-template.hbs'))
+module.exports = {
+
+  branches: [
+    "dev*"
+  ],
+  plugins: [
+    [
+      'semantic-release-gitmoji', {
+        releaseRules: {
+          major: [':boom:'],
+          minor: [':sparkles:'],
+          patch: [
+            ':bug:',
+            ':zap:',
+            ':ambulance:',
+            ':lock:',
+            ':hammer:',
+            ':white_check_mark:',
+            ':right_arrow_curving_left:',
+          ]
+        },
+        releaseNotes: {
+          template,
+          partials: {
+            commitTemplate
+          },
+          helpers: {
+            formatDate: function(date){
+              return dateFormat(date, 'yyy-mm-dd HH:MM:ss');
+            },
+            split: function(string){
+              return string.trim().split('\n');
+            },
+            formatDateCol: function(date) {
+              if(!date) date = new Date();
+              let formattedDate = date.toLocaleString('en-US', { timeZone: 'America/Bogota', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false }).replace(',','');
+              return `${formattedDate[2]}-${formattedDate[0]}-${formattedDate[1]} ${formattedDate[3]}:${formattedDate[4]}:${formattedDate[5]}`;
+            }
+          }
+        }
+      }
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        "prepareCmd": "mvn versions:set -DnewVersion=\"${nextRelease.version}\" && mvn clean install",
+        "successCmd": 'echo "Release ${nextRelease.version} published successfully"'
+      }
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md",
+        "changelogTitle": ['# Gitmoji Changelog', process.env.REPO_NAME, '\uD83C\uDF88'].join(' ')
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "**/pom.xml",
+          "docs/CHANGELOG.md"
+        ],
+        message: [
+          ':bookmark: ${nextRelease.version} [skip ci]'
+        ].join('')
+      }
+    ]
+  ],
+  tagFormat: '${version}'
+}
